199f544d7288b414325231801703e6f3d43f4a1d
Samara Melo <samara.melo299@gmail.com>
2025-10-25 14:09:10 -0300

    Add files via upload

---
+++ Estrutura_Sequencial/src/EX2.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EX2 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double raio = sc.nextDouble();
+        double pi = 3.14159;
+        double area = pi * raio * raio;
+
+        System.out.printf("A area do círculo é igual á: %.4f%n", area);
+        sc.close();
+    }
+}
