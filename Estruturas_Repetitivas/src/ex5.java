1ac27a1887b4243ad0c091a9c3de537cd1ced964
Samara Melo <samara.melo299@gmail.com>
2025-10-29 13:12:36 -0300

    Add files via upload

---
+++ Estruturas_Repetitivas/src/ex5.java
@@ -0,0 +1,16 @@
+public class ex5 {
+
+    public static void main(String[] args) {
+
+        int x = 2;
+        int y = 10;
+
+        System.out.println("Olá");
+
+        while (x < y) {
+            System.out.println(x + "-" + y);
+            x = x * 2;
+            y = y + 1;
+        }
+    }
+}
