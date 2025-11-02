9530009c9fe20cebfbd008dbd02451108ff91af0
Samara Melo <samara.melo299@gmail.com>
2025-11-02 14:41:17 -0300

    Add files via upload

---
+++ Estrutura_While/src/EX3.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+
+public class EX3 {
+
+    public static void main(String[] args) {
+
+        double x = 100;
+        double y = 100;
+
+        while (x != y) {
+            System.out.print("Olha");
+            x = Math.sqrt(y);
+        }
+    }
+}
---
+++ Estrutura_While/src/EX4.java
@@ -0,0 +1,15 @@
+public class EX4 {
+
+    public static void main(String[] args) {
+
+        int x = 0;
+        int y;
+
+        while (x < 5) {
+            y = x * 3;
+            System.out.println(y);
+            x = x +1;
+        }
+        System.out.println("Fim");
+    }
+}
