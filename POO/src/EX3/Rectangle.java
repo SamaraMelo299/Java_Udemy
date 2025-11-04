5f2e7b7b2d47634883205b21f4970b7124877c13
Samara Melo <samara.melo299@gmail.com>
2025-11-03 23:57:49 -0300

    Add files via upload

---
+++ POO/src/EX3/Rectangle.java
@@ -0,0 +1,26 @@
+package EX3;
+
+public class Rectangle {
+    public double width;
+    public double height;
+
+    public double area() {
+        return width * height;
+    }
+
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    public double diagonal() {
+        return Math.sqrt(width * width + height * height);
+    }
+
+    public String toString() {
+        return  String.format ("AREA: %.2f%n", area())
+                + String.format ("PERIMETRO: %.2f%n", perimeter())
+                + String.format ("DIAGONAL: %.2f%n", diagonal());
+    }
+
+}
+
