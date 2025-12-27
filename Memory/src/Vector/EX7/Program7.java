33ed8e2471eb6ed6e2c68f23ff0bdc6114a2b752
Samara Melo <samara.melo299@gmail.com>
2025-12-27 00:18:30 -0300

    Adiciona estrutura completa do projeto

---
+++ Memory/src/Vector/EX7/Program7.java
@@ -0,0 +1,39 @@
+package Vector.EX7;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program7 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n, posMaior;
+        double maior;
+
+        System.out.print("Quantos números você vai digitar? ");
+        n = sc.nextInt();
+
+        double[] vet = new double[n];
+
+        for (int i = 0; i < vet.length; i++) {
+            System.out.print("Digite um número: ");
+            vet[i] = sc.nextDouble();
+        }
+
+        maior = vet[0];
+        posMaior = 0;
+
+        for (int i = 1; i < vet.length; i++) {
+            if (vet[i] > maior) {
+                maior = vet[i];
+                posMaior = i;
+            }
+        }
+
+        System.out.printf("\nMaior Valor = %.1f\n", maior);
+        System.out.printf("Posicao do Maior Valor = %d\n", posMaior);
+
+    }
+}
