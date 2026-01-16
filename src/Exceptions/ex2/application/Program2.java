c584a6102fe7f093d7d2cb2b64b377ddf3a9df29
Samara Melo <samara.melo299@gmail.com>
2026-01-15 21:06:09 -0300

    utilizando try catch

--- src/Exceptions/ex2/application/Program2.java
+++ src/Exceptions/ex2/application/Program2.java
@@ -1,23 +1,31 @@
 package Exceptions.ex2.application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program2 {
 
     public static void main(String[] args) {
+
         File file = new File("C:\\temp\\in.txt");
         Scanner sc = null;
+
         try {
             sc = new Scanner(file);
             while (sc.hasNextLine()) {
                 System.out.println(sc.nextLine());
             }
         }
-        catch (IOException e) {
+        catch (FileNotFoundException e) {
             System.out.println("Error opening file: " + e.getMessage());
         }
         finally {
             if (sc != null) {
                 sc.close();
             }
+            System.out.println("Finally block executed");
         }
     }
 }
