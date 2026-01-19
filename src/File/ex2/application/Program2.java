537e877305c21f756e7c3ff196874d6c91afb760
Samara Melo <samara.melo299@gmail.com>
2026-01-19 15:51:38 -0300

    Bloco try-with-resources

---
+++ src/File/ex2/application/Program2.java
@@ -0,0 +1,26 @@
+package File.ex2.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program2 {
+    public static void main(String[] args) {
+        String path = "C:\\temp\\in.txt";
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+}
