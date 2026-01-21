455d16feca96396c1f880e1fc9436615e0ac59e9
Samara Melo <samara.melo299@gmail.com>
2026-01-20 22:20:39 -0300

    Herança múltipla + evitando problema do diamante

---
+++ src/Interfaces/ex4/devices/Printer.java
@@ -0,0 +1,6 @@
+package Interfaces.ex4.devices;
+
+public interface Printer {
+
+    void print (String doc);
+}
---
+++ src/Interfaces/ex4/devices/Scanner.java
@@ -0,0 +1,6 @@
+package Interfaces.ex4.devices;
+
+public interface Scanner {
+
+    String scan();
+}
