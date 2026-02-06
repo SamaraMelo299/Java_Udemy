b687b5020841aa26b67e9728fb1a0236a198ba96
Samara Melo <samara.melo2999@gmail.com>
2026-02-06 10:58:15 -0300

    hashcode e equals

---
+++ src/Generics/ex4/application/Program.java
@@ -0,0 +1,21 @@
+package Generics.ex4.application;
+
+import Generics.ex4.entities.Client;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Client c1 = new Client("Maria", "maria@gmail.com");
+        Client c2 = new Client("Alex", "alex@gmail.com");
+
+        String s1 = "Test";
+        String s2 = "Test";
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c1.equals(c2));
+        System.out.println(c1 == c2);
+        System.out.println(s1 == s2);
+    }
+}
