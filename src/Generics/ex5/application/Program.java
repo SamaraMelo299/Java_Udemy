fbfd135bd1145a8a026c1dad463352d18a8312f5
Samara Melo <samara.melo2999@gmail.com>
2026-02-06 11:44:57 -0300

    como as coleções Hash testam igualdade

--- src/Generics/ex5/application/Program.java
+++ src/Generics/ex5/application/Program.java
@@ -1,5 +1,6 @@
 package Generics.ex5.application;
 
+import Generics.ex5.entities.Product;
 import com.sun.source.tree.Tree;
 
 import java.util.Arrays;
@@ -11,19 +12,11 @@ public class Program {
 
     public static  void main(String[] args) {
 
-        Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
-        Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
-
-        Set<Integer> c = new TreeSet<>(a);
-        c.addAll(b);
-        System.out.println("Union: " + c);
-
-        Set<Integer> d = new TreeSet<>(a);
-        d.retainAll(b);
-        System.out.println("Intersection: " + d);
-
-        Set<Integer> e = new TreeSet<>(a);
-        e.removeAll(b);
-        System.out.println("Difference: " + e);
+        Set<Product> set = new HashSet<>();
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+        Product prod = new Product("Notebook", 1200.0);
+        System.out.println(set.contains(prod));
     }
 }
